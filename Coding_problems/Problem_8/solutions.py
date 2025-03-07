5f61dbe844bf98374e9d7a1ee2f46e3c44d9d0a6
Rithvikns <86957380+Rithvikns@users.noreply.github.com>
2025-03-07 23:32:30 +0100

    Update solutions.py

--- Coding_problems/Problem_8/solutions.py
+++ Coding_problems/Problem_8/solutions.py
@@ -6,9 +6,20 @@ def max_profit(prices):
   left_profit = [0]*n
 
   for i in range(1,n):
-    min_price = min(min_price , price[i])
-    left_profit[i] = max(left_profit[i-1] , price[i] - min_price)
+    min_price = min(min_price , prices[i])
+    left_profit[i] = max(left_profit[i-1] , prices[i] - min_price)
 
+  max_price = prices[-1]
+  right_profit = [0]*n
+
+  for i in range(n-2,-1,-1):
+    max_price = max(max_price , price[i])
+    right_profit[i] = max(right_profit[i+1] ,  max_price - prices[i])
+
+  max_profit = 0
+  for i in range(n):
+    max_profit = max(max_profit , left_profit[i] + right_profit[i])
+    return max_profit
 
 
 
