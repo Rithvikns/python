b06176019c084e5ff3d5e01aa33824f33a472942
Rithvikns <86957380+Rithvikns@users.noreply.github.com>
2025-03-07 23:27:06 +0100

    Update solutions.py

--- Coding_problems/Problem_8/solutions.py
+++ Coding_problems/Problem_8/solutions.py
@@ -1,10 +1,13 @@
 def max_profit(prices):
   if not prices:
     return 0
+  n = len(prices)
+  min_price = 0
+  left_profit = [0]*n
 
-
-
-
+  for i in range(1,n):
+    min_price = min(min_price , price[i])
+    left_profit[i] = max(left_profit[i-1] , price[i] - min_price)
 
 
 
