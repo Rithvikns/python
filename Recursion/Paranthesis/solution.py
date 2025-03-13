71dfe4aab6144a814a0d504d012c861bd990496b
Rithvikns <86957380+Rithvikns@users.noreply.github.com>
2025-03-13 22:49:16 +0100

    Update solution.py

--- Recursion/Paranthesis/solution.py
+++ Recursion/Paranthesis/solution.py
@@ -1,1 +1,8 @@
-
+def generate_paranthesis(n):
+  def recursion(n,diff,current_list):
+    if n == 0:
+      output.append("".join(current_list[:]))
+      return 
+    if diff < 0 or diff > n:
+      return
+    
